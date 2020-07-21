ca67d5edf449670fb689a4ed9bdabd9cd75dca61
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2020-07-21 18:07:53 +0300

    Imput precedures

--- PhoneBook/src/PhoneBook.java
+++ PhoneBook/src/PhoneBook.java
@@ -1,27 +1,83 @@
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
-        //Добавить считывание ввода пользователя в цикле
-    }
-
-    public static boolean checkPhoneNumber(String phoneNumber) {
-        return true;
-    }
+        String[][] book = new String [1][1];
+        Scanner scanner = new Scanner(System.in);
+        String operation = "";
+        int i = 0;
+        String name = "";
+        String number = "";
+        boolean isCorrectName;
+        boolean isCorrectNumber;
+        while (!(operation=="s")) {
+            System.out.println("Select operation");
+            System.out.println("1. Добавить адресат");
+            System.out.println("2. Просмотреть адресную книгу");
+            System.out.println("3. Выход");
+            operation = scanner.nextLine() ;
+            switch (operation) {
+                case "1":
+                    System.out.print("Введите Имя, Фамилия, Отчество (через пробел): ");
+                    name = scanner.nextLine(); //Считывает строку из System.in
+                    isCorrectName = false;
+                    while (!isCorrectName) {
+                        isCorrectName = checkName(name);
+                        if (!isCorrectName) {
+                            System.out.println("Введите корректное имя!");
+                        } //else book[0][i] = name;
+                    }
+                    System.out.print("Введите номер телефона по формату X XXX XXX XX XX: ");
+                    number = scanner.nextLine(); //Считывает строку из System.in
+                    isCorrectNumber = false;
+                    while (!isCorrectNumber) {
+                        isCorrectNumber = checkPhoneNumber(number);
+                        if (!isCorrectNumber) {
+                            System.out.println("Введите корректный номер! Номер должен содерзать 11 цифр по формату X XXX XXX XX XX");
+                        } else {
+                            System.out.println("Номер был приведен к требуему формату и записан в базу как: " + formatPhoneNumber(number));
+                            //book[1][i] = formatPhoneNumber(number);
+                        }
+                    }
+                    add(book, name, number);
+                case "s" :
+                        break;
+                default:
+                    System.out.println("Not correct input");
+                    System.out.println("Select operation");
+                    System.out.println("1. Добавить адресат");
+                    System.out.println("2. Просмотреть адресную книгу");
+                    System.out.println("3. Выход");
+                    operation = scanner.nextLine() ;
+                    break;
+            }
+            }
+        }
 
     public static boolean checkName(String name) {
-        return true;
+        String[] tmp = new String[name.length()];
+        tmp = name.split(" ");
+        return (tmp.length==3);
     }
-
     public static String formatName(String name) {
         return "";
     }
+    private static boolean checkPhoneNumber(String phoneNumber) {
+        String clean = phoneNumber.replaceAll("[^0-9]", "");
+        return clean.length() == 11;
+    }
+    private static String formatPhoneNumber(String phoneNumber) {
+        String clean = phoneNumber.replaceAll("[^0-9]", "");
+        String result = "+7" + " " + clean.substring(1, 4) + " " +
+                clean.substring(4, 7) + " " + clean.substring(7, 9) + " " + clean.substring(9);
 
-    public static String formatPhoneNumber(String number) {
-        return "";
+        return result;
     }
 
     public static void add(String[][] book, String name, String number) {
-        //add logic
+
+
     }
 
     public static void list(String[][] book) {
